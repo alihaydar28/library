ec4489fb8e81710ab42fdd896e6fc1ac7fb1e01b
alihaidar <alihaydar282828@gmail.com>
2023-01-16 10:07:20 +0200

    read+remove buttons and display

---
+++ javascript.js
@@ -0,0 +1,96 @@
+
+
+const container = document.querySelector('.maincontainer');
+
+
+
+
+
+let b1 = new Book('harry potter','JK Rowling',43,true);
+let b2 = new Book('harry potter 2','JK Rowling',600,false);
+let myLibrary=[b1,b2]; // array to store the books
+display();
+function Book(title,author,pages,readStatus){
+    // the constructor
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.readStatus=readStatus;
+}
+
+Book.prototype.changeReadStatus= function(){
+    if(this.readStatus===false){
+        this.readStatus=true;
+  
+    }else{
+        this.readStatus=false;
+        
+    }
+}
+
+/*
+function addBookToLibrary(...args){
+   // Book.apply(this,args); // args is an array here that contains title author ...
+    let b = new Book(args[0],args[1],args[2],args[3]);
+    myLibrary.push(b);
+}
+*/
+function addBookToLibrary(book){
+     myLibrary.push(book);
+ }
+
+function display(){
+ for(let i=0;i<myLibrary.length;i++){
+    let box= document.createElement('div'); // khl2t div
+    box.classList.add('box'); // aam baatiya class esma box
+    let p1=document.createElement('p');
+    let p2=document.createElement('p');
+    let p3=document.createElement('p');
+    p1.classList.add('bookname');
+    p2.classList.add('author');
+    p3.classList.add('numberpages');
+
+    let readBtn= document.createElement('button');
+    readBtn.classList.add('readBtn');
+   
+    
+    let removeBtn= document.createElement('button');
+    removeBtn.classList.add('removeBtn');
+    removeBtn.textContent="remove";
+
+    p1.textContent=myLibrary[i].title;
+    p2.textContent=myLibrary[i].author;
+    p3.textContent=myLibrary[i].pages + " pages";
+    if(myLibrary[i].readStatus===true){
+        readBtn.textContent="read";
+        readBtn.style.backgroundColor='#9ffc9c';
+    }else{
+        readBtn.textContent="unread";
+        readBtn.style.backgroundColor='#FF9c9c';
+    }
+    box.append(p1,p2,p3,readBtn,removeBtn);
+    box.dataset.index=i;
+    container.append(box);
+ }
+}
+
+container.addEventListener('click',function(e){
+    if(e.target.classList.contains('readBtn')){
+        let index = e.target.parentElement.dataset.index;
+        myLibrary[index].changeReadStatus();
+        if(myLibrary[index].readStatus===true){
+            e.target.style.backgroundColor='#9ffc9c';
+        }else{
+            e.target.style.backgroundColor='#FF9c9c';
+        }
+    }
+    else if(e.target.classList.contains('removeBtn')){
+        let index = e.target.parentElement.dataset.index;
+        container.innerHTML = "";
+        myLibrary.splice(index,1);
+        display();
+    }
+});
+
+
+
